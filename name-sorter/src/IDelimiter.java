deb58e9adfb5d643a9a5c9634b5c52960b524ffc
proandceo <proandceo@gmail.com>
2021-12-01 23:53:11 +1100

    Add files via upload

---
+++ name-sorter/src/IDelimiter.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+/**
+ * interface
+ * for DeLIMITER and sorting function to override
+ * @author rex
+ *
+ */
+public interface IDelimiter {
+	String DELIMITER = " ";
+	void sorting(List<String> nameList);
+}
